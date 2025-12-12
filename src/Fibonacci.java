bd54a1bfa2bed24df84f017149b05f128bb7b25a
Krish Vaghela <krishvaghela454@gmail.com>
2025-12-12 11:34:34 +0530

    Code Pushed

---
+++ src/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+  public static void main(String[] args) {
+    int n = 10;
+    int a = 0, b = 1;
+
+    System.out.println("Fibonacci Sequence:");
+    for (int i = 0; i < n; i++) {
+      System.out.print(a + " ");
+      int sum = a + b;
+      a = b;
+      b = sum;
+    }
+  }
+}
